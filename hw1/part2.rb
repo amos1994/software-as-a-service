b1e44dd0276c45f68c07c4bf82ead3548dce680a
Max <max@ubuntu.(none)>
2013-07-31 00:13:32 -0700

    finished part2

--- hw1/part2.rb
+++ hw1/part2.rb
@@ -6,9 +6,29 @@ def rps_result(m1, m2)
 end
 
 def rps_game_winner(game)
-  # YOUR CODE HERE
+  raise WrongNumberOfPlayersError unless game.length == 2
+  raise NoSuchStrategyError unless game[0][1].downcase.match(/[prs]/)
+  raise NoSuchStrategyError unless game[1][1].downcase.match(/[prs]/)
+  
+  strat1 = game[0][1].downcase
+  strat2 = game[1][1].downcase
+
+  if strat1 == 'p' and strat2 != 's'
+	return game[0]
+  elsif strat1 == 'r' and strat2 != 'p'
+	return game[0]
+  elsif strat1 == 's' and strat2 != 'r'
+  	return game[0]
+  end
+  return game[1]
+
 end
 
 def rps_tournament_winner(tournament)
-  # YOUR CODE HERE
+  if tournament[0][0].kind_of?(String)
+	return rps_game_winner(tournament)
+  else 
+	return rps_tournament_winner([rps_tournament_winner(tournament[0]) , rps_tournament_winner(tournament[1])])
+  end
 end
+
